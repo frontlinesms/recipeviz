ebd74a202bcadac8232e029a914825e81d764a14
Sitati Kituyi <sitati.kituyi@googlemail.com>
2014-11-14 12:00:14 +0300

    WIP

--- js/recipeviz.js
+++ js/recipeviz.js
@@ -1,10 +1,64 @@
 var RecipeViz = function() {
 	var
 	handleSubmit = function() {
-		draw(parseDiagram($('#ckbk').text()));
+		parseAndDraw($('#ckbk').val());
 	},
-	parseDiagram = function(cookbookJson) {
-		return ["st=>start: Start|past:>http://www.google.com[blank]",
+	getTriggerTypes = function(recipe) {
+		var types = [];
+		$.each(recipe.triggers, function(i, trigger) {
+			if(trigger.type == "TEXT_MESSAGE") {
+				types.push('SMS');
+			}
+			else if(trigger.type == "MISSED_CALL") {
+				types.push('Missed Call');
+			}
+			else if(trigger.type == "TIME") {
+				if(typeof trigger.data.nextTriggerTime !== 'undefined') {
+					types.push(new Date(trigger.data.nextTriggerTime).toString("on dd/m/yy hh:mm"));
+				}
+				else {
+					var str = "Every ";
+					if(trigger.data.repeatFrequency > 1) {
+						str += trigger.data.repeatFrequency;
+						if(trigger.data.scheduleType === 'DAILY') {
+							str += " days ";
+						}
+						else {
+							str += " weeks ";
+						}
+					}
+					else {
+						if(trigger.data.scheduleType === 'DAILY') {
+							str += "day ";
+						}
+						else {
+							str += "week ";
+						}
+					}
+					if(trigger.data.scheduleType === 'WEEKLY') {
+						str += "on " + trigger.data.dayOfWeek + " ";
+					}
+					str += "at " + trigger.data.hourOfDay + ":" + trigger.data.minuteOfHour;
+					types.push(str);
+				}
+			}
+		});
+		return types.join(', ');
+	}
+	parseAndDraw = function(cookbookJson) {
+		var cookbook = JSON.parse(cookbookJson);
+		$.each(cookbook.recipes, function(index, recipe) {
+			var elements = [
+			],
+			paths = [
+			];
+			elements.push("st=>start: " + recipe.name + "\n" + getTriggerTypes(recipe));
+			console.log(elements.concat(paths).join('\n'));
+			//draw(elements.concat(paths).join('\n'));
+		});
+		/*
+		[
+			"st=>start: Start|past:>http://www.google.com[blank]",
 			"e=>end: Ende|future:>http://www.google.com",
 			"op1=>operation: My Operation|past",
 			"op2=>operation: Stuff|current",
@@ -17,7 +71,9 @@ var RecipeViz = function() {
 			"cond(yes, right)->c2",
 			"cond(no)->sub1(left)->op1",
 			"c2(yes)->io->e",
-			"c2(no)->op2->e"].join('\n');
+			"c2(no)->op2->e"
+		].join('\n');
+		*/
 	},
 	draw = function(diagramDescriptor) {
 		var diagram = flowchart.parse(diagramDescriptor);
@@ -30,6 +86,7 @@ var RecipeViz = function() {
 			'text-margin': 10,
 			'font-size': 14,
 			'font-color': 'black',
+			'font-family': 'arial',
 			'line-color': 'black',
 			'element-color': 'black',
 			'fill': 'white',
@@ -51,6 +108,7 @@ var RecipeViz = function() {
 			'flowstate' : {
 				'past' : { 'fill' : '#CCCCCC', 'font-size' : 12},
 				'current' : {'fill' : 'yellow', 'font-color' : 'red', 'font-weight' : 'bold'},
+				'trigger' : {'fill' : '#abd', 'font-color' : '#333', 'font-weight' : 'bold'},
 				'future' : { 'fill' : '#FFFF99'},
 				'request' : { 'fill' : 'blue'},
 				'invalid': {'fill' : '#444444'},
